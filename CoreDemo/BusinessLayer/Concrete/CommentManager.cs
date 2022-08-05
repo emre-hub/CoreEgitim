91bb56d2261abd60dc1234c864fc9f166fa10acb
Huseyin Emre Sazak <huseyinemre.98@hotmail.com>
2022-08-05 10:26:14 +0300

    View Component Yapısı
    ViewComponent kullanmaya başlıyorum

---
+++ CoreDemo/BusinessLayer/Concrete/CommentManager.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CommentManager : ICommentService
+    {
+        ICommentDal _commentDal;
+
+        public CommentManager(ICommentDal commentDal)
+        {
+            _commentDal = commentDal;
+        }
+
+        public void CommentAdd(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        //GetList metodundaki id, veritabanindaki id.
+        //businesslayer katmanının manager sınıfları
+        //WEB UI'da Entity Framework Repository ile birlikte çağırılacak
+        //O zaman tüm katmanlar birbiriyle iletişim halinde olur
+        public List<Comment> GetList(int id) 
+        {
+            return _commentDal.GetListAll(x => x.BlogID == id);
+        }
+    }
+}
--- CoreDemo/CoreDemo/Controllers/CommentController.cs
+++ CoreDemo/CoreDemo/Controllers/CommentController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class CommentController : Controller
     {
+        //CommentController : Web UI, PresentationLayer
+        //CommentManager : BusinessLayer
+        //EfRepository : DataAccessLayer
+        CommentManager cm = new CommentManager(new EfCommentRepository());
         public IActionResult Index()
         {
             return View();
@@ -13,35 +19,12 @@ namespace CoreDemo.Controllers
         {
             return PartialView();
         }
-        /*<div class="comment-top">
-                            <h4>Yorumlar</h4>
-                            <div class="media">
-                                <img src="~/CoreBlogTema/images/t1.jpg" alt="" class="img-fluid" />
-                                <div class="media-body">
-                                    <h5 class="mt-0">Joseph Goh</h5>
-                                    <p>
-                                        Lorem Ipsum convallis diam consequat magna vulputate malesuada. id dignissim sapien velit id felis ac cursus eros.
-                                        Cras a ornare elit.
-                                    </p>
-
-                                    <div class="media mt-3">
-                                        <a class="d-flex pr-3" href="#">
-                                            <img src="~/CoreBlogTema/images/t2.jpg" alt="" class="img-fluid" />
-                                        </a>
-                                        <div class="media-body">
-                                            <h5 class="mt-0">Richard Spark</h5>
-                                            <p>
-                                                Lorem Ipsum convallis diam consequat magna vulputate malesuada. id dignissim sapien velit id felis ac cursus eros.
-                                                Cras a ornare elit.
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>*/
-        public PartialViewResult CommentListByBlog()
+    
+        public PartialViewResult CommentListByBlog(int id)
         {
-            return PartialView();
+           
+            var values = cm.GetList(id);
+            return PartialView(values);
         }
     }
 }
