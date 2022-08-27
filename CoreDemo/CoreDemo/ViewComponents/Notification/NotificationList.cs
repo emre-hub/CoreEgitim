c0907fba468e2e119ed0490be4db73365f54d260
Huseyin Emre Sazak <huseyinemre.98@hotmail.com>
2022-08-27 14:12:56 +0300

    Notification Class Added
    Notification sınıflarını oluşturdum, böylelikle notification table ekledim ve migration yaptım.

---
+++ CoreDemo/CoreDemo/ViewComponents/Notification/NotificationList.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DataAccessLayer.EntityFramework;
+using BusinessLayer.Concrete;
+
+namespace CoreDemo.ViewComponents.Notification
+{
+    public class NotificationList : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetLast3Blog();
+            return View(values);
+        }
+    }
+}
